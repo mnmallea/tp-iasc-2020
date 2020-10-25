5ee4b2f9bdc5c140fff954ee11c881edfa707cab
Martin Mallea <mnmallea@gmail.com>
2020-10-25 05:35:04 +0000

    Doble quote all strings

--- test/group_room_test.exs
+++ test/group_room_test.exs
@@ -8,8 +8,8 @@ defmodule Pigeon.GroupRoomTest do
     %{room: room}
   end
 
-  test 'creates a message', %{room: room} do
-    message_text = 'Welcome to Pigeon'
+  test "creates a message", %{room: room} do
+    message_text = "Welcome to Pigeon"
     GroupRoom.create_message(room, message_text)
 
     [received_message] = GroupRoom.list_messages(room)
@@ -18,9 +18,9 @@ defmodule Pigeon.GroupRoomTest do
     assert received_message.id
   end
 
-  test 'creates and update a message', %{room: room} do
-    old_text = 'old message text'
-    new_text = 'new message text'
+  test "creates and update a message", %{room: room} do
+    old_text = "old message text"
+    new_text = "new message text"
     GroupRoom.create_message(room, old_text)
     [%Message{id: created_id}] = GroupRoom.list_messages(room)
     GroupRoom.update_message(room, created_id, new_text)
@@ -29,8 +29,8 @@ defmodule Pigeon.GroupRoomTest do
     assert new_text == received_text
   end
 
-  test 'creates and deletes a message', %{room: room} do
-    message_text = 'some message'
+  test "creates and deletes a message", %{room: room} do
+    message_text = "some message"
     GroupRoom.create_message(room, message_text)
     [%Message{id: created_id}] = GroupRoom.list_messages(room)
 
