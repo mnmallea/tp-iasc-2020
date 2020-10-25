b1845ef860384d98712be218589f3068c05b83af
Martin Mallea <mnmallea@gmail.com>
2020-10-25 01:51:32 +0000

    Unscope GroupRoom module

--- test/group_room_test.exs
+++ test/group_room_test.exs
@@ -1,16 +1,17 @@
 defmodule Pigeon.GroupRoomTest do
   use ExUnit.Case, async: true
+  alias Pigeon.Rooms.GroupRoom
 
   setup do
-    {:ok, room} = Pigeon.Rooms.GroupRoom.start_link(nil)
+    {:ok, room} = GroupRoom.start_link(nil)
     %{room: room}
   end
 
   test 'creates a message', %{room: room} do
     message_text = 'Welcome to Pigeon'
-    Pigeon.Rooms.GroupRoom.create_message(room, message_text)
+    GroupRoom.create_message(room, message_text)
 
-    [received_message] = Pigeon.Rooms.GroupRoom.list_messages(room)
+    [received_message] = GroupRoom.list_messages(room)
 
     assert message_text == received_message
   end
