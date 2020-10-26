b9f42651884fb9f8bbb7bb9a1db5e801f61be1a4
Leandro Agustín Szikora <lszikora@dblandit.com>
2020-10-26 19:37:22 -0300

    Format code

--- lib/users/user_registry.ex
+++ lib/users/user_registry.ex
@@ -1,43 +1,44 @@
 defmodule Pigeon.UserRegistry do
-    use GenServer, Node
-    
-    def start_link(user) do
-        GenServer.start_link(__MODULE__, [], name: user)
+  use GenServer, Node
+
+  def start_link(user) do
+    GenServer.start_link(__MODULE__, [], name: user)
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:add_to_registry, node}, _from, state) do
+    {:reply, [node | state], [node | state]}
+  end
+
+  @impl true
+  def handle_call({:show_connections}, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_cast({:broadcast_message, message}, state) do
+    for node <- state do
+      Pigeon.Network.spawn_task(Pigeon.User, :print_message, node, [message])
     end
 
-    @impl true
-    def init(state) do
-        {:ok, state}
-    end
+    {:noreply, state}
+  end
 
-    @impl true
-    def handle_call({:add_to_registry, node}, _from, state) do
-        {:reply, [node | state], [node | state]}
-    end
+  def add_to_registry({user, node}) do
+    GenServer.call(user, {:add_to_registry, node})
+  end
 
-    @impl true
-    def handle_call({:show_connections}, _from, state) do
-        {:reply, state, state}
-    end
+  def show_connections({user, node}) do
+    connections = GenServer.call(user, {:show_connections})
+    Pigeon.Network.spawn_task(Pigeon.User, :print_message, node, [connections])
+  end
 
-    @impl true  
-    def handle_cast({:broadcast_message, message}, state) do
-        for node <- state do
-            Pigeon.Network.spawn_task(Pigeon.User, :print_message, node, [message])
-        end
-        {:noreply, state}
-    end
-    
-    def add_to_registry({user, node}) do
-        GenServer.call(user, {:add_to_registry, node})
-    end
-
-    def show_connections({user, node}) do
-        connections = GenServer.call(user, {:show_connections})
-        Pigeon.Network.spawn_task(Pigeon.User, :print_message, node, [connections])
-    end
-
-    def broadcast_message(user, message) do
-        GenServer.cast(user, {:broadcast_message, message})
-    end
+  def broadcast_message(user, message) do
+    GenServer.cast(user, {:broadcast_message, message})
+  end
 end
